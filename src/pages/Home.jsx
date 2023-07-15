24a6ce4fc8007621455b825d7e6bae2e58b909c5
Kamerr Ezz <kamerr_oficial@hotmail.com>
2023-07-15 14:58:47 -0600

    3.1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 
 const Home = () => {
+
   return (
-    <div>Home</div>
+    <div className='container mx-auto p-3 grid place-content-center min-h-screen'>
+      <h1 className='font-bold text-6xl text-white'>Zeew Blog</h1>
+      <button className='border border-gray-700 rounded-md px-2 py-1 text-white hover:bg-gray-700 mt-3'>
+        Aprender algo nuevo
+      </button>
+    </div>
   )
 }
 
