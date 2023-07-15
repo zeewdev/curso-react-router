b18ef28362e664c56159201df25a3ec43aa4d342
Kamerr Ezz <kamerr_oficial@hotmail.com>
2023-07-15 15:14:02 -0600

    3.3

--- src/pages/BlogDetails.jsx
+++ src/pages/BlogDetails.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 const BlogDetails = () => {
   return (
-    <div>BlogDetails</div>
+    <div className='container mx-auto p-3 text-white'>
+      <h1 className='text-5xl font-bold'>HTML Y CSS</h1>
+      <figure className='aspect-video my-5'>
+        <img className='w-full rounded-lg' src='https://cdn.discordapp.com/attachments/1038187542257872916/1093592386808053972/HTML-CSS.png' alt="" />
+      </figure>
+      <div>
+        <p className='mb-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque et, fuga animi itaque molestiae odit mollitia labore laboriosam nulla numquam. Voluptates quo labore architecto pariatur accusantium ipsam. Ex aperiam eveniet laudantium, rerum omnis itaque ad.</p>
+        <p className='mb-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque et, fuga animi itaque molestiae odit mollitia labore laboriosam nulla numquam. Voluptates quo labore architecto pariatur accusantium ipsam. Ex aperiam eveniet laudantium, rerum omnis itaque ad.</p>
+        <p className='mb-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque et, fuga animi itaque molestiae odit mollitia labore laboriosam nulla numquam. Voluptates quo labore architecto pariatur accusantium ipsam. Ex aperiam eveniet laudantium, rerum omnis itaque ad.</p>
+        <p className='mb-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque et, fuga animi itaque molestiae odit mollitia labore laboriosam nulla numquam. Voluptates quo labore architecto pariatur accusantium ipsam. Ex aperiam eveniet laudantium, rerum omnis itaque ad.</p>
+        <p className='mb-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque et, fuga animi itaque molestiae odit mollitia labore laboriosam nulla numquam. Voluptates quo labore architecto pariatur accusantium ipsam. Ex aperiam eveniet laudantium, rerum omnis itaque ad.</p>
+        <p className='mb-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque et, fuga animi itaque molestiae odit mollitia labore laboriosam nulla numquam. Voluptates quo labore architecto pariatur accusantium ipsam. Ex aperiam eveniet laudantium, rerum omnis itaque ad.</p>
+        <p className='mb-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque et, fuga animi itaque molestiae odit mollitia labore laboriosam nulla numquam. Voluptates quo labore architecto pariatur accusantium ipsam. Ex aperiam eveniet laudantium, rerum omnis itaque ad.</p>
+      </div>
+    </div>
   )
 }
 
