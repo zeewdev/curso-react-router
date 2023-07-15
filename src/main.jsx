0933a37e2b1ef4a494d51af432b7ab877880d422
Kamerr Ezz <kamerr_oficial@hotmail.com>
2023-07-15 14:27:55 -0600

    2.2

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Blogs from "./pages/Blogs";
+import BlogDetails from "./pages/BlogDetails";
+import BlogCreate from "./pages/BlogCreate";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <Routes>
+      <Route index element={<Home />} />
+      <Route path="/blogs" element={<Blogs />} />
+    </Routes>
+  </BrowserRouter>
+);
