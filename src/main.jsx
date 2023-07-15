dada36dd3f99319297e4596deac6cabb73ea229d
Kamerr Ezz <kamerr_oficial@hotmail.com>
2023-07-15 15:44:05 -0600

    4.1

--- src/main.jsx
+++ src/main.jsx
@@ -8,25 +8,31 @@ import Home from "./pages/Home";
 import Blogs from "./pages/Blogs";
 import BlogDetails from "./pages/BlogDetails";
 import BlogCreate from "./pages/BlogCreate";
+import Error from "./pages/Error";
 
-const router =  createHashRouter([
+const router = createHashRouter([
   {
-    path: '',
-    element: <Home/>
+    path: "",
+    element: <Home />,
   },
   {
-    path: '/blogs',
-    element: <Blogs/>
+    path: "/blogs",
+    element: <Blogs />,
   },
   {
-    path: '/blog/:id',
-    element: <BlogDetails/>
+    path: "/blog/:id",
+    element: <BlogDetails />,
   },
   {
-    path: '/create',
-    element: <BlogCreate/>
+    path: "/create",
+    element: <BlogCreate />,
   },
-])
+  {
+    path: "*",
+    element: <Error />,
+    errorElement: <Error />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <RouterProvider router={router} />
